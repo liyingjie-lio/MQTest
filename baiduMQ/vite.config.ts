4d4854062d01c7fa215fb9611075462674a3cab7
Morritte Klein <Kleinliojie@outlook.com>
2024-05-15 08:32:00 +0800

    前端

---
+++ baiduMQ/vite.config.ts
@@ -0,0 +1,16 @@
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+
+export default {
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver(
+          {
+            importStyle: false,
+          }
+      )],
+    }),
+  ],
+};
