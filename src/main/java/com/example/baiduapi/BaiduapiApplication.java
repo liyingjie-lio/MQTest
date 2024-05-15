34a892690b16457bfa08161b06a3474cc96e6cdf
最强鶸 <1095427011@qq.com>
2024-05-15 09:51:13 +0800

    多目标检测

---
+++ src/main/java/com/example/baiduapi/BaiduapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.baiduapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiduapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiduapiApplication.class, args);
+    }
+
+}
